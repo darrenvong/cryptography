5648128d1a611280ffc5b22059ba76d38e14561c
Darren Vong <d.vongy@gmail.com>
2016-05-19 05:21:59 +0100

    Initial commit after .gitignores

---
+++ encryption.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+
+"""
+Raw implementation of the cryptography encryption functions as taught in MAS345,
+written as another mean to consolidate understanding of the concepts in the maths module.
+@author: Darren Vong
+"""
+import itertools
+
+import numpy as np
+from numpy.linalg import det
+
+"""Standard alphanumeric conversion dictionaries (tables) for mod 26 and mod 29"""
+ALPHABET_MOD26 = ''.join([chr(i) for i in xrange(65,91)]) # 'A' starts at index 65 in ASCII in Python
+NUMS_TO_ALPHABET_MOD26 = {i: ALPHABET_MOD26[i] for i in xrange(26)}
+ALPHABET_TO_NUMS_MOD26 = dict([(v, k) for k, v in NUMS_TO_ALPHABET_MOD26.iteritems()])
+
+ALPHABET_MOD29 = ''.join([chr(i) for i in xrange(65,91)])+'_?!'
+NUMS_TO_ALPHABET_MOD29 = {i: ALPHABET_MOD29[i] for i in xrange(29)}
+ALPHABET_TO_NUMS_MOD29 = dict([(v, k) for k, v in NUMS_TO_ALPHABET_MOD29.iteritems()])
+
+def convert_message(m, table, reverse=False):
+    """Converts a numeric message m into its alphabetical representation. table is
+    the alphanumeric conversion dictionary between a message's numeric encoding and
+    alphabetical representation, depending on if reverse is True; if it is,
+    then converts an alphabetical message back to its numeric encoding instead."""
+    
+    if not reverse:
+        if not isinstance(table.keys()[0], int):
+            raise TypeError("Wrong type of conversion dictionary supplied.")
+        message_in_letters = ''.join([table[l] for l in m])
+        return message_in_letters
+    else:
+        if not isinstance(table.keys()[0], str):
+            raise TypeError("Wrong type of conversion dictionary supplied.")
+        message_in_nums = [table[i] for i in m]
+        return message_in_nums
+
+def caesar_encrypt(k, m, mod=26):
+    encrypted_m = (m + k) % mod
+    return convert_message(encrypted_m, NUMS_TO_ALPHABET_MOD26)
+
+def caesar_decrypt(k, m, mod=26):
+    decrypted_m = (m - k) % mod
+    return convert_message(decrypted_m, NUMS_TO_ALPHABET_MOD26)
+
+def affine_encrypt(K, L, m, table, mod=29):
+    """Performs an n-dimensional affine encryption on a message m given a nxn matrix K
+    and a nx1 column vector L. mod is the field in which the encryption is working in,
+    which should be in mod 26 or 29. table is the alphanumeric conversion dictionary from
+    the numerical encoding to the alphabetical representation of the message.
+    Returns the encrypted message in letters."""
+
+    encrypted_m = (np.dot(K,m) + L) % mod
+    return convert_message(encrypted_m, table)
+
+def affine_decrypt(K, L, m, table, mod=29):
+    """The inverse operation of affine_encrypt: the paramaters have exactly the same
+    meanings."""
+
+    # This is going to be quite involved as numpy doesn't do inverse in fields,
+    # don't really have time to do this for general n x n matrices so only works
+    # for simple 2x2 for now...
+    K_inv = inverse_2x2_matrix(K, mod)
+    decrypted_m = (np.dot(m, K_inv) - np.dot(L, K_inv)) % mod
+    return convert_message(decrypted_m, table)
+
+def find_inverse(x, mod):
+    """Find the inverse of x in mod 'mod' - e.g. 3^-1 in F_31 is -10 or 21"""
+    for i in itertools.count(start=1):
+        if (i * x) % mod == 0:
+            raise ArithmeticError("An inverse does not exist since 'x' and 'mod' are not coprime.")
+        elif (i * x) % mod == 1:
+            return i
+        elif i == 10**9: # Some arbitrary upper bound
+            return -1
+
+def inverse_2x2_matrix(matrix, mod):
+    inv_matrix = matrix.copy()
+    inv_matrix[0,0], inv_matrix[1,1] = inv_matrix[1,1], inv_matrix[0,0]
+    inv_matrix[0,1] *= -1
+    inv_matrix[1,0] *= -1
+    det_inv = find_inverse(int(det(matrix)), mod)
+    return (det_inv * inv_matrix) % mod
---
+++ encryption_test.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+"""
+A set of unit tests for the encryption module's various encrpytion and decryption functions.
+@author: Darren Vong
+"""
+import unittest
+
+import numpy as np
+from numpy.testing import assert_equal
+
+import encryption
+from encryption import NUMS_TO_ALPHABET_MOD26, NUMS_TO_ALPHABET_MOD29
+
+class testEncryption(unittest.TestCase):
+
+    def test_affine_encrypt(self):
+        K = np.array([[5,5],[5,8]])
+        L = [9,2]
+        m = [18,7]
+        encrypted_text = encryption.affine_encrypt(K,L,m,NUMS_TO_ALPHABET_MOD29)
+        expected = 'SD'
+        self.assertEqual(encrypted_text, expected)
+
+    def test_affine_encrypt_1D(self):
+        k, l = 9, 19
+        m = np.array([18,7,14,14,19])
+        encrypted_text = encryption.affine_encrypt(k,l,m,NUMS_TO_ALPHABET_MOD26,mod=26)
+        expected = 'ZEPPI'
+        self.assertEqual(encrypted_text, expected)
+
+    def test_caesar_encrypt(self):
+        k = 16
+        m = np.array([18,19,14,15])
+        encrypted_text = encryption.caesar_encrypt(k,m)
+        expected = 'IJEF'
+        self.assertEqual(encrypted_text, expected)
+
+    def test_caesar_decrypt(self):
+        k = 16
+        e_m = np.array([19,7,4,5])
+        decrypted_text = encryption.caesar_decrypt(k, e_m)
+        self.assertEqual(decrypted_text, 'DROP')
+
+    def test_inv_2x2(self):
+        M = np.array([[5,5],[5,8]])
+        M_inv = encryption.inverse_2x2_matrix(M, 29)
+        expected = np.array([[16,19],[19,10]])
+        assert_equal(M_inv, expected)
+
+    def test_find_inverse(self):
+        inv = encryption.find_inverse(15,29)
+        self.assertEqual(inv, 2)
+
+    def test_affine_decrypt(self):
+        K = np.array([[5,5],[5,8]])
+        L, M = [9,2], [20,12]
+        decrypted_text = encryption.affine_decrypt(K, L, M, NUMS_TO_ALPHABET_MOD29)
+        expected = 'ST'
+        self.assertEqual(decrypted_text, expected)
+
+if __name__ == "__main__":
+    unittest.main()
